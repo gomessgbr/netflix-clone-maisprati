9c06d3ee7ba0a535bc7a61eaec935b225ce46996
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-08-25 13:43:37 -0300

    wip: navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,27 @@
-import logo from "../../assets/netflix-logo.svg";
-
 export function NavBar() {
   return (
-    <>
-      <img src={logo} alt="" />
-    </>
+    <div className="z-10 ">
+      <a href="#"></a>
+      <ul>
+        <li>
+          <a href="#">Início</a>
+        </li>
+        <li>
+          <a href="#">Séries</a>
+        </li>
+        <li>
+          <a href="#">Filmes</a>
+        </li>
+        <li>
+          <a href="#">Bombando</a>
+        </li>
+        <li>
+          <a href="#">Minha Lista</a>
+        </li>
+      </ul>
+      <div>
+        <input type="search" name="" id="" />
+      </div>
+    </div>
   );
 }
