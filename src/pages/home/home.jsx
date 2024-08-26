a04c2e894643b63f444e6bfbce658993ecf61bfe
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-08-26 20:17:39 -0300

    feat: video in home

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,13 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { NavBar } from "../../components";
 import { MovieList } from "./components/MovieList";
-import { useGetMovies } from "./hooks/useGetMovies";
+// import { useGetMovies } from "./hooks/useGetMovies";
 import { useGetCollection } from "./hooks/useGetColletion";
 import { MovieCard } from "./components/MovieCard";
+import trailer from "../../assets/trailer/sex-education.mp4";
+
+// TODO - Implementar o video de preview na home
+// TODO - Fazer a parte das telas, quando clicar carregar cada uma das categorias da páginas
+// TODO - Dar erro quando os inputs da tela de login forem vazios
 
 export function Home() {
   const [query, setQuery] = useState("");
-  const { movies } = useGetMovies();
+  // const { movies } = useGetMovies();
   const { getColletion, moviesCollection } = useGetCollection();
 
   const onSearch = (e) => {
@@ -16,13 +21,23 @@ export function Home() {
     setQuery(inputSearchValue);
   };
 
+  useEffect(() => {}, []);
+
   return (
     <div className="bg-black min-h-screen min-w-[100dvw] ">
       <NavBar onSearch={onSearch} />
-      <></>
+      <div className="flex h-2/5 bg-black">
+        <video className="w-full " autoPlay muted loop>
+          <source src={trailer} type="video/mp4" />
+        </video>
+      </div>
+
       {query === "" && (
         <div className="pb-40">
-          <MovieList title={"Tendencia agora!"} data={movies} />
+          <MovieList title={"Tendências"} />
+          <MovieList title={"Bem Avaliados"} />
+          <MovieList title={"Em Breve"} />
+          <MovieList title={"Em Cartaz"} />
         </div>
       )}
       {query !== "" && (
@@ -30,15 +45,13 @@ export function Home() {
           {moviesCollection &&
             moviesCollection.map((movie) => (
               <MovieCard
-                key={movie.id}
+                key={movie.id.toString()}
                 url={movie.backdrop_path}
                 id={movie.id}
                 title={movie.name}
               />
             ))}
-            {
-              !moviesCollection && <p>Não encontrados</p>
-            }
+          {!moviesCollection && <p>Não encontrados</p>}
         </div>
       )}
     </div>
