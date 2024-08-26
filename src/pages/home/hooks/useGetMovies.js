19ac79e4c3ef660e1ced2966e7eddda16d481132
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-08-26 20:25:44 -0300

    refactor: if for not type

--- src/pages/home/hooks/useGetMovies.js
+++ src/pages/home/hooks/useGetMovies.js
@@ -1,26 +1,28 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { requestClient } from "../../../service/api/requestClient";
 
 export function useGetMovies() {
   const [movies, setMovies] = useState([]);
 
-  const getMovies = async () => {
-    try {
-      const response = await requestClient(
-        "get",
-        "movie/popular?language=pt-BR&page=1"
-      );
-      setMovies(response);
-    } catch (error) {
-      console.error("Error fetching movie image: ", error);
-    }
+  const getMovies = async (type) => {
+      if(type){
+        try {
+          const response = await requestClient(
+            "get",
+            `movie/${type}?language=pt-BR&page=1`
+          );
+          setMovies(response.results);
+        } catch (error) {
+          console.error("Error fetching movie image: ", error);
+        }
+
+      }
   };
 
-  useEffect(() => {
-    getMovies();
-  }, []);
+
 
   return {
     movies,
+    getMovies,
   };
 }
