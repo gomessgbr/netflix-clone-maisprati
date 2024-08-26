cee06b7919041cc36913b24edb99a2996f675aa1
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-08-25 21:10:42 -0300

    feat: request service

---
+++ src/service/api/config.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const BASE_URL = "https://api.themoviedb.org/";
+const authToken =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNDA2MDY2MjJmNGMzYmExZWU3OGY2MjZhZTlkNGIxNiIsIm5iZiI6MTcyNDYxMzA5My4xOTYyNzgsInN1YiI6IjY2YzY4MzdjOWUzNGQxZmU3ZmZhNDc0ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bl7TJT9b4IlDqstXfR3wVlZJ4lbo6BHuHzIU_FdOQEI";
+
+export const api = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    Authorization: `Bearer ${authToken}`,
+  },
+});
