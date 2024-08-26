508700deff004018909fe1c46930b3cbd0b3b835
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-08-25 21:13:52 -0300

    wip: home screen

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,7 +1,16 @@
 import { NavBar } from "../../components";
+import { MovieList } from "./components/MovieList";
+import { useGetMovies } from "./hooks/useGetMovies";
 
 export function Home() {
+  const { movies } = useGetMovies();
+
   return (
-    <NavBar/>
-  )
+    <div className="bg-black min-h-screen min-w-[100dvw] ">
+      <NavBar />
+      <div className="pb-40">
+        <MovieList title={"Tendencia agora!"} data={movies} />
+      </div>
+    </div>
+  );
 }
