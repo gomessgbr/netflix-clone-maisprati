fd1d8ac4d9bad1e8a7fe9a4303ce79a939576d0c
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-08-25 21:13:09 -0300

    feat: get movie hook

---
+++ src/pages/home/hooks/useGetMovieImage.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { requestClient } from "../../../service/api/requestClient";
+
+export function useGetMovieImage() {
+  const [image, setImage] = useState();
+
+  const getImage = async (idMovie) => {
+    try {
+      const response = await requestClient(
+        "get",
+        `/3/movie/${idMovie}/images?include_image_language=pt&language=pt-BR`
+      );
+      if (response.posters.length > 0) {
+        console.log('teste')
+        setImage(response.posters[0]);
+      }
+    } catch (error) {
+      console.error("Error fetching movie image: ", error);
+    }
+  };
+
+  return {
+    image,
+    getImage
+  };
+}
